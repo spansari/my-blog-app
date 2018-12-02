57b0549a6190ef756a4a2cd5b27a4af41eae7712
Sanjiv Pansari <sanjiv.pansari@macys.com>
2018-12-02 13:40:19 -0500

    Post update delete feature implemented

--- blog/forms.py
+++ blog/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, ValidationError
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, ValidationError, TextAreaField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 from blog.models import User
 from flask_login import current_user
@@ -43,11 +43,15 @@ class UpdateAccountnForm(FlaskForm):
         if username.data != current_user.username:
             user = User.query.filter_by(username=username.data).first()
             if user:
-                raise ValidationError('That username is take. Please choose a different one.')
+                raise ValidationError('That username is taken. Please choose a different one.')
 
     def validate_email(self, email):
         if email.data != current_user.email:
             user = User.query.filter_by(email=email.data).first()
             if user:
-                raise ValidationError('That email is take. Please choose a different one.')
+                raise ValidationError('That email is taken. Please choose a different one.')
 
+class PostForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    content = TextAreaField('Content', validators=[DataRequired()])
+    submit = SubmitField('Post')
